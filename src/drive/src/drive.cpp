052440230c62a821366905e785975c238363e44c
youngjju <2000dudwn@naver.com>
2025-08-06 20:43:25 +0900

    feat: 좌우측 차선의 중심선 계산

--- src/drive/src/drive.cpp
+++ src/drive/src/drive.cpp
@@ -22,6 +22,7 @@ class DriveNode : public rclcpp::Node
         void process_frame()
         {
             cv::Mat frame, mask, birdview;
+            cv::vector<cv::Point> center_points;
             cam_ >> frame;
             if(frame.empty()){
                 RCLCPP_WARN(this->get_logger(), "빈 프레임이 입력되었습니다.");
@@ -52,6 +53,12 @@ class DriveNode : public rclcpp::Node
 
             cv::warpPerspective(mask, birdview, matrix_Trans, mask.size()); // 버드아이뷰 변환 (srcPts/dstPts 조정 혹은 카메라 조정 혹은 해상도 조정)
 
+            center_points = extract_centerline(birdview); // 좌측, 우측 차선의 중심선 벡터 계산
+
+            for (const auto& pt : center_points) {
+                cv::circle(birdview, pt, 3, cv::Scalar(128), -1); // 중심선은 imshow에서 회색(Scalar128)로 보임
+            }
+
             cv::imshow("상태", birdview);
 
             if(cv::waitKey(1)==27){
@@ -59,19 +66,47 @@ class DriveNode : public rclcpp::Node
             }
         }
 
-        cv::Mat applyRoi(const cv::Mat& input, std::vector<cv::Point> polygon){
-            int height = input.rows;
-            int width = input.cols;
+        // cv::Mat applyRoi(const cv::Mat& input, std::vector<cv::Point> polygon){
+        //     int height = input.rows;
+        //     int width = input.cols;
             
-            cv::Mat mask = cv::Mat::zeros(input.size(), input.type());
+        //     cv::Mat mask = cv::Mat::zeros(input.size(), input.type());
             
-            std::vector<std::vector<cv::Point>> polygons{polygon};
-            cv::fillPoly(mask, polygons, cv::Scalar(255));
+        //     std::vector<std::vector<cv::Point>> polygons{polygon};
+        //     cv::fillPoly(mask, polygons, cv::Scalar(255));
 
-            cv::Mat masked;
-            cv::bitwise_and(input, mask, masked);
+        //     cv::Mat masked;
+        //     cv::bitwise_and(input, mask, masked);
 
-            return masked;
+        //     return masked;
+        // }
+
+        std::vector<cv::Point> extract_centerline(const cv::Mat& birdview)
+        {
+            std::vector<cv::Point> center_points;
+            
+            int height = birdview.rows;
+            int width = birdview.cols;
+
+            int step = 10;
+            
+            for(int y = height - 1; y>=0; y-=step)
+            {
+                std::vector<int> x_positions;
+
+                for(int x=0; x<width; ++x){
+                    if(birdview.at<uchar>(y,x)>200){
+                        x_positions.push_back(x);
+                    }
+                }
+                
+                if(x_positions.size()>2){
+                    int sum = std::accumulate(x_positions.begin(), x_positions.end(), 0);
+                    int avg_x = sum/ static_cast<int>(x_positions.size());
+                    center_points.push_back(cv::Point(avg_x, y));
+                }
+            }
+            return center_points;
         }
 
         cv::VideoCapture cam_;
