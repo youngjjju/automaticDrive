b97606db4b221bd730b9ba0d0782e794adc5efd7
youngjju <2000dudwn@naver.com>
2025-08-06 18:34:53 +0900

    feat: Gaussian Blur, Canny Edge 등 적용

--- src/drive/src/drive.cpp
+++ src/drive/src/drive.cpp
@@ -6,23 +6,45 @@ class DriveNode : public rclcpp::Node
     public: 
         DriveNode(): Node("drive_node")
         {
-            cv::VideoCapture cam(0);
+            cam_.open(0);
             if(!cam.isOpened()){
                 RCLCPP_ERROR(this->get_logger(), "카메라를 열 수 없습니다");
+                return;
             }
 
-            cv::Mat frame;
-            while(rclcpp::ok()){
-                cam >> frame;
-                cv::imshow("카메라 상태", frame);
-
-                // 여기
-
-                if(cv::waitKey(10) ==27) break;
-            }
-            cam.release();
-            cv::destroyAllWindows();
+            timer_ = this->create_wall_timer(
+                std::chrono::milliseconds(33),
+                std::bind(&DriveNode::process_frame, this)
+            );
         }
+
+    private:
+        void process_frame()
+        {
+            cv::Mat frame, mask, edges;
+            cam_ >> frame;
+            if(frame.empty()){
+                RCLCPP_WARN(this->get_logger(), "빈 프레임이 입력되었습니다.");
+                return;
+            }   
+            
+            cv::GaussianBlur(frame, frame, cv::Size(5,5),0); // 가우시안블러 (경계선을 선명하게 함, Size 조정 가능)
+            
+            cv::Scalar lower_white(220, 220, 220);
+            cv::Scalar upper_white(255, 255,255);  
+            cv::inRange(frame, lower_white, upper_white, mask);  // 흰색 선만 추출 (lower_white 값 조정 가능)
+
+            cv::Canny(mask, edges, 100, 200); // Canny 엣지 검출 (임계값은 튜닝 가능)
+
+            cv::imshow("카메라 상태", edges);
+
+            if(cv::waitKey(1)==27){
+                rclcpp::shutdown();
+            }
+        }
+
+        cv::VideoCapture cam_;
+        rclcpp::TimerBase::SharedPtr timer_;
 };
 
 int main(int argc, char **argv)
